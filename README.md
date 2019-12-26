dfafad66531028eeabb0b00b7872ef729f00de7a
aferriss <aferriss@gmail.com>
2019-12-26 13:01:19 -0500

    add readme and gif

---
+++ README.md
@@ -0,0 +1,5 @@
+### Look At example
+
+![gif](gifLook.gif)
+
+This is a little project showing how to use the lookAt API in Spark AR. Most of the example is located in the script.js file. There is also a small utility function that you may copy and use as you wish.
--- lookAtExample/scripts/script.js
+++ lookAtExample/scripts/script.js
@@ -2,7 +2,39 @@ const S = require("Scene");
 const R = require("Reactive");
 const Time = require("Time");
 
+// Finds an element in the scene
+// - Parameters:
+//      e: The object to find
 const find = e => S.root.find(e);
+
+// Gets the position of an object as an R.point
+// - Parameters:
+//      e: The object to get the transform from
+const getPosition = e => R.point(e.transform.x, e.transform.y, e.transform.z);
+
+// Sets the rotation based on a transform
+// - Parameters:
+//      e: The object to rotate
+//      p: The transform to use
+const setRotation = (e, p) => {
+  e.transform.rotationX = p.rotationX;
+  e.transform.rotationY = p.rotationY;
+  e.transform.rotationZ = p.rotationZ;
+};
+
+// Look at utility function.
+// Because of reactive stupidness, we can't actually apply the lookat directly to the looker itself
+// We get around this by nesting the looker object inside a null with no values applied to it's transform
+//
+// - Parameters:
+//      _target: The object in the scene you want to face
+//      _lookerParent: The parent object of the object you want to rotate. Should have no transform applied to it
+//      _looker: The object that you want to rotate towards the target
+const lookAt = (_target, _lookerParent, _looker) => {
+  const ptToLookAt = getPosition(_target);
+  const lookAtTransform = _lookerParent.transform.lookAt(ptToLookAt);
+  setRotation(_looker, lookAtTransform);
+};
 
 const plane = find("plane0");
 const followNull = find("followNull");
@@ -14,10 +46,13 @@ targetNull.transform.x = R.sin(Time.ms.mul(R.val(0.001))).mul(scl);
 targetNull.transform.y = R.cos(Time.ms.mul(R.val(0.0007))).mul(scl);
 targetNull.transform.z = R.sin(Time.ms.mul(R.val(0.0005))).mul(scl);
 
-// Lookat
-const lookAtPt = R.point(targetNull.transform.x, targetNull.transform.y, targetNull.transform.z);
-const lookAtTransform = followNull.transform.lookAt(lookAtPt);
+// Do the look at
+lookAt(targetNull, followNull, plane);
 
-plane.transform.rotationX = lookAtTransform.rotationX;
-plane.transform.rotationY = lookAtTransform.rotationY;
-plane.transform.rotationZ = lookAtTransform.rotationZ;
+// Here's a simpler implementation without the helper functions
+// const lookAtPt = R.point(targetNull.transform.x, targetNull.transform.y, targetNull.transform.z);
+// const lookAtTransform = followNull.transform.lookAt(lookAtPt);
+
+// plane.transform.rotationX = lookAtTransform.rotationX;
+// plane.transform.rotationY = lookAtTransform.rotationY;
+// plane.transform.rotationZ = lookAtTransform.rotationZ;
