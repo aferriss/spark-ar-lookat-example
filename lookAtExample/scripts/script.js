730905067ae3f8f26e1409c24608307429ae55c8
aferriss <aferriss@gmail.com>
2019-12-26 12:34:44 -0500

    change world transform to transform and added a cone

--- lookAtExample/scripts/script.js
+++ lookAtExample/scripts/script.js
@@ -1,6 +1,5 @@
 const S = require("Scene");
 const R = require("Reactive");
-const D = require("Diagnostics");
 const Time = require("Time");
 
 const find = e => S.root.find(e);
@@ -10,10 +9,15 @@ const followNull = find("followNull");
 const targetNull = find("targetNull");
 
 // Random animation
-targetNull.transform.x = R.sin(Time.ms.mul(R.val(0.001))).mul(0.5);
-targetNull.transform.y = R.cos(Time.ms.mul(R.val(0.0007))).mul(0.5);
-targetNull.transform.z = R.sin(Time.ms.mul(R.val(0.0005))).mul(0.5);
+const scl = R.val(0.1);
+targetNull.transform.x = R.sin(Time.ms.mul(R.val(0.001))).mul(scl);
+targetNull.transform.y = R.cos(Time.ms.mul(R.val(0.0007))).mul(scl);
+targetNull.transform.z = R.sin(Time.ms.mul(R.val(0.0005))).mul(scl);
 
 // Lookat
-const lookAtPt = R.point(targetNull.worldTransform.x, targetNull.worldTransform.y, targetNull.worldTransform.z);
-plane.transform = followNull.worldTransform.lookAt(lookAtPt);
+const lookAtPt = R.point(targetNull.transform.x, targetNull.transform.y, targetNull.transform.z);
+const lookAtTransform = followNull.transform.lookAt(lookAtPt);
+
+plane.transform.rotationX = lookAtTransform.rotationX;
+plane.transform.rotationY = lookAtTransform.rotationY;
+plane.transform.rotationZ = lookAtTransform.rotationZ;
